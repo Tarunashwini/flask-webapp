ff60235934b7cd41b0a8ff7b89bc55551597afb9
Bharath <bharath@hotmail.com>
2024-06-12 12:37:25 +0530

    commit 4 by Bharath

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def hello_world():
-    return '<h1 style="color:blue; text-align:center;">Hello, Tarun Ashwini. Commit 3 </h1>'
+    return '<h1 style="color:blue; text-align:center;">Hello, Tarun Ashwini. Commit 4 </h1>'
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
